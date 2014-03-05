c3a67e202632fd57ce9f262ab8abe4160df38e81
Ted Young <tyoung@pivotallabs.com>
2014-03-04 16:19:36 -0800

    CONSTANTIZE_ROUTES
    Signed-off-by: Onsi Fakhouri <onsi@pivotallabs.com>

--- router/file_server_routes.go
+++ router/file_server_routes.go
@@ -1,8 +1,13 @@
 package router
 
+const (
+	FS_STATIC         = "static"
+	FS_UPLOAD_DROPLET = "upload_droplet"
+)
+
 func NewFileServerRoutes() Routes {
 	return Routes{
-		{Path: "/droplet/:guid", Method: "POST", Handler: "upload_droplet"},
-		{Path: "/static/", Method: "GET", Handler: "static"},
+		{Path: "/static/", Method: "GET", Handler: FS_STATIC},
+		{Path: "/droplet/:guid", Method: "POST", Handler: FS_UPLOAD_DROPLET},
 	}
 }
