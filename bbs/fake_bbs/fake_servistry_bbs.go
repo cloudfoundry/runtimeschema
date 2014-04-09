67823ba0105fa392620b1824dbf7f56d4ff7ee64
Ted Young <tyoung@pivotallabs.com>
2014-04-09 11:53:18 -0700

    make fake_servistry_bbs synchronous
    Signed-off-by: Phan Le <ple@pivotallabs.com>

--- bbs/fake_bbs/fake_servistry_bbs.go
+++ bbs/fake_bbs/fake_servistry_bbs.go
@@ -10,33 +10,37 @@ type RegisterCCInputs struct {
 	Ttl          time.Duration
 }
 
-type FakeServistryBBS struct {
-	RegisterCCInputs  chan RegisterCCInputs
-	RegisterCCOutputs chan error
+type UnregisterCCInputs struct {
+	Registration models.CCRegistrationMessage
+}
 
-	UnregisterCCInputs  chan models.CCRegistrationMessage
-	UnregisterCCOutputs chan error
+type FakeServistryBBS struct {
+	RegisterCCInputs  []RegisterCCInputs
+	RegisterCCOutputs struct {
+		Err error
+	}
+
+	UnregisterCCInputs  []UnregisterCCInputs
+	UnregisterCCOutputs struct {
+		Err error
+	}
 }
 
 func NewFakeServistryBBS() *FakeServistryBBS {
-	return &FakeServistryBBS{
-		RegisterCCInputs:    make(chan RegisterCCInputs),
-		RegisterCCOutputs:   make(chan error),
-		UnregisterCCInputs:  make(chan models.CCRegistrationMessage),
-		UnregisterCCOutputs: make(chan error),
-	}
+	return &FakeServistryBBS{}
 }
 
 func (bbs *FakeServistryBBS) RegisterCC(registration models.CCRegistrationMessage, ttl time.Duration) error {
-	bbs.RegisterCCInputs <- RegisterCCInputs{
+	bbs.RegisterCCInputs = append(bbs.RegisterCCInputs, RegisterCCInputs{
 		Registration: registration,
 		Ttl:          ttl,
-	}
-
-	return <-bbs.RegisterCCOutputs
+	})
+	return bbs.RegisterCCOutputs.Err
 }
 
 func (bbs *FakeServistryBBS) UnregisterCC(registration models.CCRegistrationMessage) error {
-	bbs.UnregisterCCInputs <- registration
-	return <-bbs.UnregisterCCOutputs
+	bbs.UnregisterCCInputs = append(bbs.UnregisterCCInputs, UnregisterCCInputs{
+		Registration: registration,
+	})
+	return bbs.UnregisterCCOutputs.Err
 }
