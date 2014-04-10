539e72ae07e5538fbf7df3faa35793b2e7cce9ce
Karl Isenberg <kisenberg@pivotallabs.com>
2014-04-10 15:16:31 -0700

    upload build artifact route
    Signed-off-by: Ted Young <tyoung@pivotallabs.com>

--- router/file_server_routes.go
+++ router/file_server_routes.go
@@ -1,13 +1,16 @@
 package router
 
 const (
-	FS_STATIC         = "static"
-	FS_UPLOAD_DROPLET = "upload_droplet"
+	FS_STATIC                   = "static"
+	FS_UPLOAD_DROPLET           = "upload_droplet"
+	FS_UPLOAD_BUILD_ARTIFACTS   = "upload_build_artifacts"
+	FS_DOWNLOAD_BUILD_ARTIFACTS = "download_build_artifacts"
 )
 
 func NewFileServerRoutes() Routes {
 	return Routes{
 		{Path: "/static/", Method: "GET", Handler: FS_STATIC},
 		{Path: "/droplet/:guid", Method: "POST", Handler: FS_UPLOAD_DROPLET},
+		{Path: "/build_artifacts/:app_guid", Method: "PUT", Handler: FS_UPLOAD_BUILD_ARTIFACTS},
 	}
 }
