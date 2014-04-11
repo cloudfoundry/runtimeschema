e9a53d54eeffdb7efbf535a316918590a5f2ca17
Ted Young <tyoung@pivotallabs.com>
2014-04-10 17:43:40 -0700

    added download build artifact route
    Signed-off-by: Karl Isenberg <kisenberg@pivotallabs.com>

--- router/file_server_routes.go
+++ router/file_server_routes.go
@@ -12,5 +12,6 @@ func NewFileServerRoutes() Routes {
 		{Path: "/static/", Method: "GET", Handler: FS_STATIC},
 		{Path: "/droplet/:guid", Method: "POST", Handler: FS_UPLOAD_DROPLET},
 		{Path: "/build_artifacts/:app_guid", Method: "PUT", Handler: FS_UPLOAD_BUILD_ARTIFACTS},
+		{Path: "/build_artifacts/:app_guid", Method: "GET", Handler: FS_DOWNLOAD_BUILD_ARTIFACTS},
 	}
 }
