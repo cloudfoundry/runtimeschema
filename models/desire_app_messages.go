7746b3f3715d209dbf0a6af25fc850dcb814a7be
Alex Suraci <asuraci@pivotallabs.com>
2014-05-14 11:23:04 -0700

    add stack to desire app request from CC
    Signed-off-by: Ted Young <tyoung@pivotallabs.com>

--- models/desire_app_messages.go
+++ models/desire_app_messages.go
@@ -4,6 +4,7 @@ type DesireAppRequestFromCC struct {
 	AppId        string                `json:"app_id"`
 	AppVersion   string                `json:"app_version"`
 	DropletUri   string                `json:"droplet_uri"`
+	Stack        string                `json:"stack"`
 	StartCommand string                `json:"start_command"`
 	Environment  []EnvironmentVariable `json:"environment"`
 }
