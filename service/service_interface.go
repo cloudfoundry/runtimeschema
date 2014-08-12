24d5b489794ba7fa006464d00865773e227c612b
Ted Young <tyoung@pivotallabs.com>
2014-08-12 16:18:26 -0700

    remove deprecated code so we can write it again
    Signed-off-by: Alex Suraci <asuraci@pivotallabs.com>

--- service/service_interface.go
+++
@@ -1,9 +0,0 @@
-package service
-
-// The Service interface contains all the behavior a component must implement in
-// order to be considered a proper service.
-type Service interface {
-	Start(onPrematureStop func()) error
-	Stop() error
-	AwaitShutdown()
-}
