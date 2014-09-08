1e53dcc881e45048a12ef687ceacb10d2553aad1
Abhijit Hiremagalur <abhi@pivotallabs.com>
2014-09-08 12:05:34 -0700

    StagingMetadata => ExectutionMetadata in DesireAppRequestFromCC
    - Since this metadata is used to run the app not stage it
    - Onsi suggested this in a tracker comment
    - [#78232658]

--- cc_messages/desired_messages.go
+++ cc_messages/desired_messages.go
@@ -3,19 +3,19 @@ package cc_messages
 import "encoding/json"
 
 type DesireAppRequestFromCC struct {
-	ProcessGuid     string      `json:"process_guid"`
-	DropletUri      string      `json:"droplet_uri"`
-	DockerImageUrl  string      `json:"docker_image"`
-	Stack           string      `json:"stack"`
-	StartCommand    string      `json:"start_command"`
-	StagingMetadata string      `json:"staging_metadata"`
-	Environment     Environment `json:"environment"`
-	MemoryMB        int         `json:"memory_mb"`
-	DiskMB          int         `json:"disk_mb"`
-	FileDescriptors uint64      `json:"file_descriptors"`
-	NumInstances    int         `json:"num_instances"`
-	Routes          []string    `json:"routes"`
-	LogGuid         string      `json:"log_guid"`
+	ProcessGuid       string      `json:"process_guid"`
+	DropletUri        string      `json:"droplet_uri"`
+	DockerImageUrl    string      `json:"docker_image"`
+	Stack             string      `json:"stack"`
+	StartCommand      string      `json:"start_command"`
+	ExecutionMetadata string      `json:"execution_metadata"`
+	Environment       Environment `json:"environment"`
+	MemoryMB          int         `json:"memory_mb"`
+	DiskMB            int         `json:"disk_mb"`
+	FileDescriptors   uint64      `json:"file_descriptors"`
+	NumInstances      int         `json:"num_instances"`
+	Routes            []string    `json:"routes"`
+	LogGuid           string      `json:"log_guid"`
 }
 
 func (d DesireAppRequestFromCC) ToJSON() []byte {
