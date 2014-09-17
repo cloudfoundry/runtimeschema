7d7020e7014c058e914b9a45a2646cafe195a65d
Alex Suraci <asuraci@pivotallabs.com>
2014-09-16 17:37:41 -0700

    add Mebibytes and Metric metrics
    "Metric" is an actual Metrics 2.0 unit used for generic numbers
    
    Signed-off-by: Amit Gupta <agupta@pivotallabs.com>

--- metric/metric.go
+++ metric/metric.go
@@ -21,3 +21,15 @@ type Duration string
 func (name Duration) Send(duration time.Duration) {
 	metrics.SendValue(string(name), float64(duration), "nanos")
 }
+
+type Mebibytes string
+
+func (name Mebibytes) Send(mebibytes int) {
+  metrics.SendValue(string(name), float64(mebibytes), "MiB")
+}
+
+type Metric string
+
+func (name Metric) Send(mebibytes int) {
+  metrics.SendValue(string(name), float64(mebibytes), "Metric")
+}
