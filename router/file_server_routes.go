311f96c7eaa60adba74d2c5cc11242e409584983
Michael Fraenkel <fraenkel@us.ibm.com>
2014-10-25 13:28:22 -0400

    Remove download build artifact ability
    [#80393104]

--- router/file_server_routes.go
+++ router/file_server_routes.go
@@ -1,10 +1,9 @@
 package router
 
 const (
-	FS_STATIC                   = "static"
-	FS_UPLOAD_DROPLET           = "upload_droplet"
-	FS_UPLOAD_BUILD_ARTIFACTS   = "upload_build_artifacts"
-	FS_DOWNLOAD_BUILD_ARTIFACTS = "download_build_artifacts"
+	FS_STATIC                 = "static"
+	FS_UPLOAD_DROPLET         = "upload_droplet"
+	FS_UPLOAD_BUILD_ARTIFACTS = "upload_build_artifacts"
 )
 
 func NewFileServerRoutes() Routes {
@@ -12,6 +11,5 @@ func NewFileServerRoutes() Routes {
 		{Path: "/v1/static/", Method: "GET", Handler: FS_STATIC},
 		{Path: "/v1/droplet/:guid", Method: "POST", Handler: FS_UPLOAD_DROPLET},
 		{Path: "/v1/build_artifacts/:app_guid", Method: "POST", Handler: FS_UPLOAD_BUILD_ARTIFACTS},
-		{Path: "/v1/build_artifacts/:app_guid", Method: "GET", Handler: FS_DOWNLOAD_BUILD_ARTIFACTS},
 	}
 }
