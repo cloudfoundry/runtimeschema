5bbb41cc62f3d0bb046f8d05f0c1ca346e207013
Alex Suraci <asuraci@pivotal.io>
2014-12-10 14:27:13 -0800

    add health check types
    [#83638700]
    
    Signed-off-by: Atul Kshirsagar <atul.kshirsagar@gmail.com>

--- cc_messages/desired_messages.go
+++ cc_messages/desired_messages.go
@@ -2,20 +2,27 @@ package cc_messages
 
 import "encoding/json"
 
+type HealthCheckType string
+
+const UnspecifiedHealthCheckType HealthCheckType = "" // backwards-compatibility
+const PortHealthCheckType HealthCheckType = "port"
+const NoneHealthCheckType HealthCheckType = "none"
+
 type DesireAppRequestFromCC struct {
-	ProcessGuid       string      `json:"process_guid"`
-	DropletUri        string      `json:"droplet_uri"`
-	DockerImageUrl    string      `json:"docker_image"`
-	Stack             string      `json:"stack"`
-	StartCommand      string      `json:"start_command"`
-	ExecutionMetadata string      `json:"execution_metadata"`
-	Environment       Environment `json:"environment"`
-	MemoryMB          int         `json:"memory_mb"`
-	DiskMB            int         `json:"disk_mb"`
-	FileDescriptors   uint64      `json:"file_descriptors"`
-	NumInstances      int         `json:"num_instances"`
-	Routes            []string    `json:"routes"`
-	LogGuid           string      `json:"log_guid"`
+	ProcessGuid       string          `json:"process_guid"`
+	DropletUri        string          `json:"droplet_uri"`
+	DockerImageUrl    string          `json:"docker_image"`
+	Stack             string          `json:"stack"`
+	StartCommand      string          `json:"start_command"`
+	ExecutionMetadata string          `json:"execution_metadata"`
+	Environment       Environment     `json:"environment"`
+	MemoryMB          int             `json:"memory_mb"`
+	DiskMB            int             `json:"disk_mb"`
+	FileDescriptors   uint64          `json:"file_descriptors"`
+	NumInstances      int             `json:"num_instances"`
+	Routes            []string        `json:"routes"`
+	LogGuid           string          `json:"log_guid"`
+	HealthCheckType   HealthCheckType `json:"health_check_type"`
 }
 
 func (d DesireAppRequestFromCC) ToJSON() []byte {
