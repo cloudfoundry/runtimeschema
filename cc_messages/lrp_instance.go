f563e7f75804fb16c0e748aae8374796f245fe92
Kris Hicks <krishicks@gmail.com>
2014-12-10 17:53:46 -0800

    Add LRPInstanceState
    (Taken from tps)
    
    [#83638702]
    
    Signed-off-by: Eric Malm <emalm@pivotal.io>

---
+++ cc_messages/lrp_instance.go
@@ -0,0 +1,17 @@
+package cc_messages
+
+type LRPInstanceState string
+
+const (
+	LRPInstanceStateStarting LRPInstanceState = "STARTING"
+	LRPInstanceStateRunning  LRPInstanceState = "RUNNING"
+	LRPInstanceStateUnknown  LRPInstanceState = "UNKNOWN"
+)
+
+type LRPInstance struct {
+	ProcessGuid  string           `json:"process_guid"`
+	InstanceGuid string           `json:"instance_guid"`
+	Index        uint             `json:"index"`
+	State        LRPInstanceState `json:"state"`
+	Since        int64            `json:"since_in_ns"`
+}
