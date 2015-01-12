f02ad6cf2100a092cdc0964fa929c0006e017028
Kris Hicks <krishicks@gmail.com>
2015-01-12 11:19:52 -0800

    support health_check_timeout_in_seconds
    [#79892828]
    
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>

--- cc_messages/desired_messages.go
+++ cc_messages/desired_messages.go
@@ -9,20 +9,21 @@ const PortHealthCheckType HealthCheckType = "port"
 const NoneHealthCheckType HealthCheckType = "none"
 
 type DesireAppRequestFromCC struct {
-	ProcessGuid       string          `json:"process_guid"`
-	DropletUri        string          `json:"droplet_uri"`
-	DockerImageUrl    string          `json:"docker_image"`
-	Stack             string          `json:"stack"`
-	StartCommand      string          `json:"start_command"`
-	ExecutionMetadata string          `json:"execution_metadata"`
-	Environment       Environment     `json:"environment"`
-	MemoryMB          int             `json:"memory_mb"`
-	DiskMB            int             `json:"disk_mb"`
-	FileDescriptors   uint64          `json:"file_descriptors"`
-	NumInstances      int             `json:"num_instances"`
-	Routes            []string        `json:"routes"`
-	LogGuid           string          `json:"log_guid"`
-	HealthCheckType   HealthCheckType `json:"health_check_type"`
+	ProcessGuid                 string          `json:"process_guid"`
+	DropletUri                  string          `json:"droplet_uri"`
+	DockerImageUrl              string          `json:"docker_image"`
+	Stack                       string          `json:"stack"`
+	StartCommand                string          `json:"start_command"`
+	ExecutionMetadata           string          `json:"execution_metadata"`
+	Environment                 Environment     `json:"environment"`
+	MemoryMB                    int             `json:"memory_mb"`
+	DiskMB                      int             `json:"disk_mb"`
+	FileDescriptors             uint64          `json:"file_descriptors"`
+	NumInstances                int             `json:"num_instances"`
+	Routes                      []string        `json:"routes"`
+	LogGuid                     string          `json:"log_guid"`
+	HealthCheckType             HealthCheckType `json:"health_check_type"`
+	HealthCheckTimeoutInSeconds uint            `json:"health_check_timeout_in_seconds"`
 }
 
 func (d DesireAppRequestFromCC) ToJSON() []byte {
