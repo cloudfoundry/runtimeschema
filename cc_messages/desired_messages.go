49ba99f2d74ed0f36b7b6671d569507dc3fead38
Michael Fraenkel <fraenkel@us.ibm.com>
2015-01-16 16:33:30 -0800

    Add EgressRules to Desired request from CC
    [#79330246]
    
    Signed-off-by: Hristo Iliev <hsiliev@gmail.com>

--- cc_messages/desired_messages.go
+++ cc_messages/desired_messages.go
@@ -1,6 +1,10 @@
 package cc_messages
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/cloudfoundry-incubator/runtime-schema/models"
+)
 
 type HealthCheckType string
 
@@ -9,22 +13,23 @@ const PortHealthCheckType HealthCheckType = "port"
 const NoneHealthCheckType HealthCheckType = "none"
 
 type DesireAppRequestFromCC struct {
-	ProcessGuid                 string          `json:"process_guid"`
-	DropletUri                  string          `json:"droplet_uri"`
-	DockerImageUrl              string          `json:"docker_image"`
-	Stack                       string          `json:"stack"`
-	StartCommand                string          `json:"start_command"`
-	ExecutionMetadata           string          `json:"execution_metadata"`
-	Environment                 Environment     `json:"environment"`
-	MemoryMB                    int             `json:"memory_mb"`
-	DiskMB                      int             `json:"disk_mb"`
-	FileDescriptors             uint64          `json:"file_descriptors"`
-	NumInstances                int             `json:"num_instances"`
-	Routes                      []string        `json:"routes"`
-	LogGuid                     string          `json:"log_guid"`
-	HealthCheckType             HealthCheckType `json:"health_check_type"`
-	HealthCheckTimeoutInSeconds uint            `json:"health_check_timeout_in_seconds"`
-	ETag                        string          `json:"etag"`
+	ProcessGuid                 string                     `json:"process_guid"`
+	DropletUri                  string                     `json:"droplet_uri"`
+	DockerImageUrl              string                     `json:"docker_image"`
+	Stack                       string                     `json:"stack"`
+	StartCommand                string                     `json:"start_command"`
+	ExecutionMetadata           string                     `json:"execution_metadata"`
+	Environment                 Environment                `json:"environment"`
+	MemoryMB                    int                        `json:"memory_mb"`
+	DiskMB                      int                        `json:"disk_mb"`
+	FileDescriptors             uint64                     `json:"file_descriptors"`
+	NumInstances                int                        `json:"num_instances"`
+	Routes                      []string                   `json:"routes"`
+	LogGuid                     string                     `json:"log_guid"`
+	HealthCheckType             HealthCheckType            `json:"health_check_type"`
+	HealthCheckTimeoutInSeconds uint                       `json:"health_check_timeout_in_seconds"`
+	EgressRules                 []models.SecurityGroupRule `json:"egress_rules,omitempty"`
+	ETag                        string                     `json:"etag"`
 }
 
 type CCDesiredStateServerResponse struct {
