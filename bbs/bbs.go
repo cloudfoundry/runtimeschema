ddb083fcf2489f246e0739205acea95abb61283e
Onsi Fakhouri <onsijoe@gmail.com>
2015-02-11 12:04:41 -0800

    give veritas groups

--- bbs/bbs.go
+++ bbs/bbs.go
@@ -152,7 +152,7 @@ type VeritasBBS interface {
 
 	//lrp
 	DesiredLRPs() ([]models.DesiredLRP, error)
-	ActualLRPs() ([]models.ActualLRP, error)
+	ActualLRPGroups() ([]models.ActualLRP, error)
 	DesireLRP(lager.Logger, models.DesiredLRP) error
 	RemoveDesiredLRPByProcessGuid(logger lager.Logger, guid string) error
 
