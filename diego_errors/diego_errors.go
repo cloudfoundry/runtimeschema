e09656d40f5209627397cf96fec912dacfc075fb
Matthew Sykes <matthew.sykes@gmail.com>
2015-03-16 14:56:43 -0700

    Add diego error for missing lifecycle data
    [#87265198]
    
    Signed-off-by: Eric Malm <emalm@pivotal.io>

--- diego_errors/diego_errors.go
+++ diego_errors/diego_errors.go
@@ -4,6 +4,7 @@ const (
 	INSUFFICIENT_RESOURCES_MESSAGE        = "insufficient resources"
 	MISSING_APP_BITS_DOWNLOAD_URI_MESSAGE = "missing app bits download uri"
 	MISSING_APP_ID_MESSAGE                = "missing app id"
+	MISSING_LIFECYCLE_DATA_MESSAGE        = "missing lifecycle data"
 	MISSING_TASK_ID_MESSAGE               = "missing task id"
 	NO_COMPILER_DEFINED_MESSAGE           = "no compiler defined for requested stack"
 	CELL_MISMATCH_MESSAGE                 = "found no compatible cell"
