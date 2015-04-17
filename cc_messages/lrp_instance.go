12a66d5155bcbdc4aed1c5c4700a61012bbdbb65
Atul Kshirsagar <atul.kshirsagar@gmail.com>
2015-04-17 12:00:56 -0700

    add metrics to LRPInstance #[91560998]
    Signed-off-by: Leo Chu <leocchu@gmail.com>

--- cc_messages/lrp_instance.go
+++ cc_messages/lrp_instance.go
@@ -10,10 +10,17 @@ const (
 )
 
 type LRPInstance struct {
-	ProcessGuid  string           `json:"process_guid"`
-	InstanceGuid string           `json:"instance_guid"`
-	Index        uint             `json:"index"`
-	State        LRPInstanceState `json:"state"`
-	Details      string           `json:"details,omitempty"`
-	Since        int64            `json:"since_in_ns"`
+	ProcessGuid  string            `json:"process_guid"`
+	InstanceGuid string            `json:"instance_guid"`
+	Index        uint              `json:"index"`
+	State        LRPInstanceState  `json:"state"`
+	Details      string            `json:"details,omitempty"`
+	Since        int64             `json:"since_in_ns"`
+	Stats        *LRPInstanceStats `json:"stats,omitempty"`
+}
+
+type LRPInstanceStats struct {
+	CpuPercentage float64 `json:"cpu"`
+	MemoryBytes   uint64  `json:"mem"`
+	DiskBytes     uint64  `json:"disk"`
 }
