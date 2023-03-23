dc5742af1a8920400b50ea2a29b4982bc2f68bec
Geoff Franks <gfranks@vmware.com>
2023-03-23 15:56:11 +0000

    Convert to ginkgo v2
    Co-authored-by: Seth Boyles <sboyles@vmware.com>

--- cc_messages/flags/lifecycles_test.go
+++ cc_messages/flags/lifecycles_test.go
@@ -3,7 +3,7 @@ package flags_test
 import (
 	"code.cloudfoundry.org/runtimeschema/cc_messages/flags"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
